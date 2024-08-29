199b949bf0a25670fa58fad6d42c142578ed5d55
syahreza01 <140473385+syahreza01@users.noreply.github.com>
2024-08-29 18:19:56 +0700

    Add files via upload

---
+++ Resa_Project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.resa_project1;
+
+/**
+ *
+ * @author USER
+ */
+public class Resa_Project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama  : Syahresa");
+        System.out.println("NPM   : 2340506079");
+        System.out.println("Alamat: Cacaban, Magelang");
+        System.out.println("No_hp : 085878801602");
+    }
+}
